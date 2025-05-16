5a52960a6edcc36d67323d6fead9b20a672a6322
canary40 <emanueledamore090@gmail.com>
2025-05-16 10:16:53 +0200

    chromeinstall.ps1

--- chromeinstall.ps1
+++ chromeinstall.ps1
@@ -1,1 +1,11 @@
-$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+$LocalTempDir = $env:TEMP
+$ChromeInstallerPath = Join-Path $LocalTempDir "chrome_installer.exe"
+
+Invoke-WebRequest -Uri "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $ChromeInstallerPath
+
+$process = Start-Process -FilePath $ChromeInstallerPath -ArgumentList "/silent", "/install" -PassThru
+$process.WaitForExit()
+
+Remove-Item $ChromeInstallerPath -Force -ErrorAction SilentlyContinue
